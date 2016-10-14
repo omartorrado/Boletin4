eabe2dcb1e8f5819103e01ab5914be0d8dbe1766
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-10-14 12:35:45 +0200

    Terminados ejercicios 1 a 3

--- src/boletin4/Boletin4.java
+++ src/boletin4/Boletin4.java
@@ -21,21 +21,33 @@ public class Boletin4 {
         //selector de ejercicio
         switch (ejercicio){
         case 1: 
-        Coche coche1 = new Coche();
-        System.out.println(coche1.getVelocidad());
-        coche1.acelerar(110);
-        System.out.println(coche1.getVelocidad());
-        coche1.acelerar(50);
-        System.out.println(coche1.getVelocidad());
-        coche1.frenar(100);
-        System.out.println(coche1.getVelocidad());
-        break;
+            Coche coche1 = new Coche();
+            System.out.println(coche1.getVelocidad());
+            coche1.acelerar(110);
+            System.out.println(coche1.getVelocidad());
+            coche1.acelerar(50);
+            System.out.println(coche1.getVelocidad());
+            coche1.frenar(100);
+            System.out.println(coche1.getVelocidad());
+            break;
         case 2:
-        //Ejercicio 2
-        Satelite s1= new Satelite();
-        s1.verPosicion();
-        Satelite s2=new Satelite(21.45,31.87,1958746);
-        s2.verPosicion();
+            //Ejercicio 2
+            Satelite s1= new Satelite();
+            s1.verPosicion();
+            Satelite s2=new Satelite(21.45,31.87,1958746);
+            s2.verPosicion();
+            break;
+        case 3:
+            //ejercicio 3
+            Circulo c1=new Circulo();
+            System.out.println("el radio es "+c1.getRadio());
+            c1.setRadio(Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor del radio del circulo")));
+            System.out.println("la longitud es de "+c1.calcularLongitud());
+            System.out.println("el area es de "+c1.calcularArea());
+            Circulo c2 = new Circulo(10);
+            System.out.println(c2.calcularArea());
+            System.out.println(c2.calcularLongitud());
+            break;
         default: break;
         
     }
---
+++ src/boletin4/Circulo.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin4;
+
+/**
+ *
+ * @author otorradomiguez
+ */
+public class Circulo {
+    
+    private double radio;
+    //lo declaramos final por ser una constante
+    private final double PI=3.14;
+    
+    public Circulo(){
+        radio=0;
+    }
+    
+    public Circulo(double r){
+        radio=r;
+    }
+    
+    public void setRadio(double r){
+        radio=r;
+    }
+    
+    public double getRadio(){
+        return radio;
+    }
+    
+    public double calcularArea(){
+        return (Math.pow(radio,2)*PI);
+    }
+    
+    public double calcularLongitud(){
+        return (2*PI*radio);
+    }
+}
